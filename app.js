a045d258371214e79a86ae222c05b7463428120a
manishshrestha01 <shresthamanish8080@gmail.com>
2026-04-20 03:30:09 +0545

    fix undefined issue

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const connectToDatabase = require('./database')
 const app = express()
+app.use(express.json())
 
 connectToDatabase()
 
@@ -12,9 +13,10 @@ app.get("/",(req,res)=>{
 }
 )
 
-app.get("/about",(req,res)=>{
-    res.json({
-        message:"This is about page"
+app.post("/blog",(req,res)=>{
+    console.log(req.body)
+    res.status(200).json({
+        message : "Blog API hit success"
     })
 })
 
